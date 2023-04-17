a9d4a0e3b3454c61148d858348965f59c3250926
moshuiyanlanjin <1078314344@qq.com>
2023-04-17 22:42:09 +0800

    枝干一次zlc

--- app/src/main/java/com/example/xiechengmvvmdemo1/MainActivity.kt
+++ app/src/main/java/com/example/xiechengmvvmdemo1/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.vm = viewModel
 
-        binding.bt.setOnClickListener { Log.d("zlc","打印的MainActivity主干00000")
+        binding.bt.setOnClickListener { Log.d("zlc","打印的MainActivity支干00000")
             viewModel.requestLogin("Derry-vip", "123456")
         }
 
