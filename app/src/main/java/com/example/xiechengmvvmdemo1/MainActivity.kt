e078afdf9ad456d54d5424b8b8391fb067d69d64
moshuiyanlanjin <1078314344@qq.com>
2023-04-17 21:59:51 +0800

    主二次

--- app/src/main/java/com/example/xiechengmvvmdemo1/MainActivity.kt
+++ app/src/main/java/com/example/xiechengmvvmdemo1/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.vm = viewModel
 
-        binding.bt.setOnClickListener { Log.d("zlc","打印的MainActivity")
+        binding.bt.setOnClickListener { Log.d("zlc","打印的MainActivity主干")
             viewModel.requestLogin("Derry-vip", "123456")
         }
 
